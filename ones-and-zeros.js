d6e563218a2b10e07ec12b6d8f53fef6a4cd3930
Carl Furrow <hello@carlfurrow.com>
2021-04-15 08:14:19 -0400

    Use dynamic programming to solve ones-and-zero(e)s
    I'm still not fully grokking this.

--- ones-and-zeros.js
+++ ones-and-zeros.js
@@ -1,4 +1,7 @@
 /*
+LeetCode April 2021 Challenge
+https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/593/week-1-april-1st-april-7th/3694/
+
 You are given an array of binary strings strs and two integers m and n.
 
 Return the size of the largest subset of strs such that there are at most m 0's and n 1's in the subset.
@@ -26,29 +29,43 @@ Explanation: The largest subset is {"0", "1"}, so the answer is 2.
  * @return {number}
  */
 var findMaxForm = function(strs, m, n) {
-  var sets = [];
-
-  for(var i = 1; i <= strs.length; i++) {
-    var setSize = i;
-    for(var j = 0; j < strs.length; j++) {
-      var currentIndex = j;
-      var tmp = [];
-      tmp.push(strs[currentIndex]);
-      for(var k = 0; k < strs.length; k++) {
-        if(tmp.indexOf(strs[k]) < 0 && tmp.length + 1 <= setSize) {
-          tmp.push(strs[k])
-        }
-      }
-      sets.push(tmp);
+  // Introducing a "dp" array, dynamic programming, to start counting stuff.
+  let dp = [];
+  for(var i = 0; i < m+1; i++) {
+    let row = [];
+    for(var j = 0; j < n+1; j++) {
+      row.push(0);
     }
+    dp.push(row);
   }
 
-  console.log("sets", sets)
-  console.log("sets.length", sets.length)
+  // The dp array stores... what? what does this 2D array store?
+  // It's organized so there are m rows, and n columns....
+  // And dp[0][0] will always be 0, as there are never going to be strings in the set with neither a 0 or a 1.
+  // dp[1][1] will be a count of... something. where zeroes is 1 and ones is 1
 
+  strs.forEach( (s) =>{
+    let zeroes = s.match(/0/g);
+    zeroes = zeroes ? zeroes.length : 0;
+    let ones = s.match(/1/g);
+    ones = ones ? ones.length : 0;
+
+    console.log({s, zeroes, ones})
+    for(var i = m; i > zeroes - 1; i--) {
+      for(var j = n; j > ones - 1; j--) {
+        //console.log(`dp[${i}][${j}]: ${dp[i][j]} versus dp[${i}-${zeroes}][${j}-${ones}]+1 (dp[${i-zeroes}][${j-ones}]): ${dp[i-zeroes][j-ones]+1}`)
+        console.log(`Find the max`)
+        dp[i][j] = Math.max(dp[i][j], dp[i-zeroes][j-ones] + 1);
+      }
+    }
+  } );
+
+  //console.log(dp);
+
+  return dp[m][n];
 };
 
-assert( findMaxForm(["10","0001","111001","1","0"], 5, 3), 4 );
+// assert( findMaxForm(["10","0001","111001","1","0"], 5, 3), 4 );
 assert( findMaxForm(["10","0","1"], 1, 1), 2 );
 
 function assert(value, expected) {
